5461859070f55ca198a2defe2f483406be0fe442
Rodrigo <digo.baiocchi@gmail.com>
2024-01-28 21:06:31 -0500

    adding #[derive(Debut)] to help with debugging printing details of structs and its values

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,15 @@
+// using #[derive(Debug)] before a struct helps us debugging code printing the struct values
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = (30, 50);
     let rect2 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50
     };
 
@@ -19,13 +22,18 @@ fn main() {
         "The area of the rectangle 2 is {} square pixels.",
         area_with_structs(&rect2)
     );
+
+    println!("Rect2 is {:#?}", &rect2);
+    // dbg! is another way to print structs to help with debugging
+    dbg!(&rect2);
 }
 
-
+// makes the code more concise but not as clear
 fn area_with_tuples(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
 
+// makes the code not as concise tuples but still clear
 fn area_with_structs(rectangle: &Rectangle) -> u32 {
     rectangle.height * rectangle.width
 }
