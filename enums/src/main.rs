b4dc5f9b1dec24ac72874f73ada513856f867ead
Rodrigo <digo.baiocchi@gmail.com>
2024-01-30 21:27:56 -0500

    adding first exercises of enums chapter

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+/**
+ * Enums can hava multiple types within
+ * */ 
+
+enum IpAddrKind {
+    v4(String),
+    v6(String),
 }
+
+enum IpAddr {
+    v4(u8, u8, u8, u8),
+    v6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::v4;
+    let six = IpAddrKind::v6;
+
+    route(IpAddrKind::v4(String::from("127.0.0.1")));
+    route(IpAddrKind::v6(String::from("::1")));
+
+    let home = IpAddr::v4(127, 0, 0, 1);
+    let loopback = IpAddr::v6(String::from("::1"));
+}
+
+fn route(ip_kind: IpAddrKind) {}
