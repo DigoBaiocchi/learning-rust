0b48e9f6a35a15b01220d1c87f444b64d3f30589
Rodrigo <digo.baiocchi@gmail.com>
2024-02-15 21:19:40 -0500

    adding examples on how to add enums to vectors

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,19 +1,21 @@
 /**
  * Vectors can only store values of the same type.
- * 
  * A vector Vec<T> can hold any type.
+ * 
+ * The following code will throw an error because when you create a vector
+ * some space in memory is allocated for it, and when a new
+ * number is added to the vector, the currect memory is deallocated and new
+ * memory is allocated for the updated vector. So when you try to print
+ * the variable first, it would be trying to read deallocated memory. 
+ * let mut v = vec![1, 2, 3, 4 , 5];
+ * let first = &v[0];
+ * v.push(6);
+ * println!("The first element is: {first}");
+ * 
+ * Enums can be used as a way to add different types to a vector.
+ * The enum, with different types, would be added to the vector and all values
+ * would be the type enum.
  * */ 
- /**
-  * The following code will throw an error because when you create a vector
-  * some space in memory is allocated for it, and when a new
-  * number is added to the vector, the currect memory is deallocated and new
-  * memory is allocated for the updated vector. So when you try to print
-  * the variable first, it would be trying to read deallocated memory. 
-  * let mut v = vec![1, 2, 3, 4 , 5];
-  * let first = &v[0];
-  * v.push(6);
-  * println!("The first element is: {first}");
-  * */ 
 
 fn main() {
     let mut v: Vec<i32> = Vec::new();
@@ -45,5 +47,17 @@ fn main() {
         Some(&second) => println!("{}", second),
         None => println!("Nothing"),
     }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 
 }
