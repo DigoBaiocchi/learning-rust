20c7f677268df5c35306cded9ea153ed2dda2349
Rodrigo <digo.baiocchi@gmail.com>
2024-02-29 21:31:44 -0500

    Chapter 9.2 notes propagating the error
    Adding notes about propagating the error from chapter 9.2

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -3,14 +3,70 @@
  * trying to access index 99 of a vector that only has 3 characters.
  * panic can also be added to the code.
  * 
+ * The result enum is defined as having Ok and Err variants.
+ * 
+ * When a function's implementation calls something that might fail,
+ * instead of handling the error within the function itself, we can
+ * return the error to the calling code so that it can decide what
+ * to do. This is known as propagating the error
  * 
  * 
- * 
- * 
- **/ 
+ **/
+
+use std::{fs::File, io::{self, ErrorKind, Read}}; 
 
 fn main() {
-    let v = vec![1,2,3];
+    let _v = vec![1,2,3];
 
-    v[99];
+    // v[99];
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let _greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(_e) => panic!("Problem creating the file: {:?}", error),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+    // logic above can be rewrite without match statements
+    let _greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // we can also use unwrap for a very short version and still catches the error
+    let _greeting_file = File::open("hello3.txt").unwrap();
+
+    // using expect will display our error message
+    let _greeting_file = File::open("hello4.txt")
+        .expect("hello.txt should be included in this project");
+}
+
+// propagating error example
+fn _read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
