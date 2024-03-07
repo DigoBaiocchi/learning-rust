a36a0abcdd3eba3418a988056932e72881bfa53a
Rodrigo <digo.baiocchi@gmail.com>
2024-03-06 21:37:26 -0500

    Finishing chapter 9.2 notes and examples
    Adding notes from chapter 9.2 about ? symbol when it can be used

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -13,10 +13,25 @@
  * The ? symbol can also be used to replace the match statement.
  * It should be placed after the statement we want to do.
  * 
+ * There's no need to call Err when using ? symbol because this symbol
+ * calls the from function and the error type received is converted into
+ * the error type defined in the return type of the current function.
+ * 
+ * The ? symbol can only be used in function whose return type is compatible 
+ * with the value of ? is used on. Because of this, it can't be used 
+ * in a main function. Unless we change the return type of the main function to
+ * for example, if we want to return a Result, the main return type would be 
+ * like Result <(), Box<dyn Error>>. The Box<dyn Error> mean "any kind of error".
+ * 
+ * When we use ? in a function that returns an Option, if the value is None,
+ * the None will be returned early from the function at that point. If the 
+ * value is Some, the value inside the Some is the resulting value of the
+ * expressionand and the function continues.
+ * 
  * 
  **/
 
-use std::{fs::File, io::{self, ErrorKind, Read}}; 
+use std::{fs::{self, File}, io::{self, ErrorKind, Read}}; 
 
 fn main() {
     let _v = vec![1,2,3];
@@ -80,4 +95,21 @@ fn _read_user_from_file_2() -> Result<String, io::Error> {
     let mut username = String::new();
     username_file.read_to_string(&mut username)?;
     Ok(username)
+}
+
+fn _read_user_from_file_3() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username);
+
+    Ok(username)
+}
+
+fn _read_user_from_file_4() -> Result<String, io::Error> {
+    fs::read_to_string("Hello.text")
+}
+
+// using ? with function that returns an Option
+fn _last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
